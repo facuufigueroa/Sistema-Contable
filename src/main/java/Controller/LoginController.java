a163201b2da44271c6a5f91d1e78254d53c3b5fc
Fernando <fernando_andana@hotmail.com>
2022-09-01 01:43:50 -0300

    Se agregaron las clases RegisterController y ViewFuntionality, y la vista registro-user. Se mejoró la vista del login y del registro. Se Agrego la funcionalidad de para minimizar y cerrar ventanas.

--- src/main/java/Controller/LoginController.java
+++ src/main/java/Controller/LoginController.java
@@ -1,18 +1,27 @@
 package Controller;
 import Model.User;
+import Model.ViewFuntionality;
 import Services.ServiceLogin;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
-public class LoginController  implements Alerta{
 
+import java.io.IOException;
+
+public class LoginController extends ViewFuntionality implements Alerta{
     //Atributos
     @FXML private Button botonIniciarSesion;
     @FXML private PasswordField campoContrasena;
@@ -20,17 +29,14 @@ public class LoginController  implements Alerta{
     @FXML private Button btnMinimize;
     @FXML private Button btnClose;
     @FXML private AnchorPane frameLogin;
+    @FXML private Text botonRegistrarse;
 
     private User user;
-
     private Stage stage;
-
     private ServiceLogin serviceLogin = new ServiceLogin();
-
+    private RegisterController registerController;
 
     //Getters y Setters
-
-
     public Stage getStage() {
         return stage;
     }
@@ -63,6 +69,10 @@ public class LoginController  implements Alerta{
     public void setCampoUsuario(TextField campoUsuario) { this.campoUsuario = campoUsuario; }
     public User getUser() { return user; }
     public void setUser(User user) { this.user = user; }
+    public Text getBotonRegistrarse() { return botonRegistrarse; }
+    public void setBotonRegistrarse(Text botonRegistrarse) { this.botonRegistrarse = botonRegistrarse; }
+    public RegisterController getRegisterController() { return registerController; }
+    public void setRegisterController(RegisterController registerController) { this.registerController = registerController; }
 
     //Metodos
     private String obtenerEmail(){ return getCampoUsuario().getText(); }
@@ -80,6 +90,22 @@ public class LoginController  implements Alerta{
     public void accionBotonIniciarSesion(){
         if (estanCamposVacios()){ alertaCamposIncompletos(); }
         else { alertaEmailContrasena(); }
+    }
+    @FXML
+    public void accionRegistrarUsuario(MouseEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/View/registro-user.fxml"));
+        Parent parent = fxmlLoader.load();
+        setRegisterController(loadRegister(fxmlLoader.getController()));
+        Scene scene = new Scene(parent);
+        Stage stage = new Stage();
+        Stage loginStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        getRegisterController().setVentana(loginStage);
+        getRegisterController().hideStage();
+        stage.setScene(scene);
+        stage.setTitle("Registrarse");
+        stage.getIcons().add(new Image(getClass().getResourceAsStream("/Images/Icono.png")));
+        stage.initStyle(StageStyle.TRANSPARENT);
+        stage.showAndWait();
     }
 
     /**
@@ -130,18 +156,6 @@ public class LoginController  implements Alerta{
         alert.showAndWait();
     }
 
-    /* Metodos para btns de login */
-    @FXML
-    public void btnClose(){ System.exit(0); }
-    @FXML
-    public void min(MouseEvent event){
-        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        stage.setIconified(true);
-    }
-    @FXML
-    public void max(MouseEvent event){
-        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        stage.setFullScreenExitHint(" ");
-        stage.setFullScreen(true);
-    }
+    /**Metodo en el cual se obtiene el controlador de la vista registro-user**/
+    private RegisterController loadRegister(RegisterController controller){ return controller; }
 }
