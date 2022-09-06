a90f09145b551012a497ecfbbc8f94f86f3814b0
facuufigueroa <figueroafacundo123@gmail.com>
2022-09-06 10:47:15 -0300

    Listar forma ascendente

--- src/main/java/Querys/CuentaQuery.java
+++ src/main/java/Querys/CuentaQuery.java
@@ -3,11 +3,11 @@ package Querys;
 public class CuentaQuery {
 
     public static String listarCuentasHabilitadas(){
-        return "SELECT * FROM cuentas as c WHERE c.estado=true";
+        return "SELECT * FROM cuentas as c WHERE c.estado=true ORDER BY c.codigo ASC";
     }
 
     public static String listarCuentasDeshabilitadas(){
-        return "SELECT * FROM cuentas as c WHERE c.estado=false";
+        return "SELECT * FROM cuentas as c WHERE c.estado=false ORDER BY c.codigo ASC";
     }
 
     public static String habilitarCuenta(String codigo){
